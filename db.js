b5a6deeb11fe241356d6fa70c6e3349b4125cba3
meetbx <panarameet69@gmail.com>
2026-08-09 20:13:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
-require('dotenv').config({ override: false });
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const isProduction = process.env.NODE_ENV === 'production';
 
