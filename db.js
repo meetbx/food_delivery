979001d6f6cee0bbbf13e9206295ea71f6f5ab44
meetbx <panarameet69@gmail.com>
2026-08-09 19:30:56 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
+
+
+
 
 module.exports = pool;
