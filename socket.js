67df1fdd265698e9d3cd09a3ead7218268ecdef5
meetbx <panarameet69@gmail.com>
2026-08-30 18:29:01 +0530

    Update socket.js

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ const {
   removeDriverLocation 
 } = require('./services/deliveryService');
 // At the top of backend/socket.js
-const pool = require('./db'); // Adjust path if your DB config file is located elsewhere (e.g., './config/db')
+const { pool } = require('./db');// Adjust path if your DB config file is located elsewhere (e.g., './config/db')
 
 let io;
 
